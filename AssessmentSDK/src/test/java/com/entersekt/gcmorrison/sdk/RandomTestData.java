66c0cd3c5f1455df31dd3ddf339ac351a28bd4fe
Campbell Morrison <cammorr@gmail.com>
2020-03-08 15:32:26 +0200

    Implemented Repository layer which allows for querying and filtering of data from the API

--- AssessmentSDK/src/test/java/com/entersekt/gcmorrison/sdk/RandomTestData.java
+++ AssessmentSDK/src/test/java/com/entersekt/gcmorrison/sdk/RandomTestData.java
@@ -1,9 +1,13 @@
 package com.entersekt.gcmorrison.sdk;
 
+import com.entersekt.gcmorrison.sdk.api.model.City;
+import com.entersekt.gcmorrison.sdk.api.model.Mall;
+import com.entersekt.gcmorrison.sdk.api.model.Shop;
+
 import java.util.UUID;
 
 /**
- * A helper interface that generates random primitive data
+ * A helper interface that generates random data for testing purposes
  */
 public interface RandomTestData {
     default int randomInt() {
@@ -12,5 +16,27 @@ public interface RandomTestData {
 
     default String randomString() {
         return UUID.randomUUID().toString();
+    }
+
+    default City randomCity(Mall... malls) {
+        return new City.Builder().setId(randomInt())
+                .setName(randomString())
+                .addMalls(malls)
+                .build();
+    }
+
+    default Mall randomMall(Shop... shops) {
+        return new Mall.Builder()
+                .setId(randomInt())
+                .setName(randomString())
+                .addShops(shops)
+                .build();
+    }
+
+    default Shop randomShop() {
+        return new Shop.Builder()
+                .setId(randomInt())
+                .setName(randomString())
+                .build();
     }
 }
---
+++ AssessmentSDK/src/test/java/com/entersekt/gcmorrison/sdk/repository/RepositoryTest.java
@@ -0,0 +1,211 @@
+package com.entersekt.gcmorrison.sdk.repository;
+
+import com.entersekt.gcmorrison.sdk.RandomTestData;
+import com.entersekt.gcmorrison.sdk.api.EntersektApi;
+import com.entersekt.gcmorrison.sdk.api.model.City;
+import com.entersekt.gcmorrison.sdk.api.model.Mall;
+import com.entersekt.gcmorrison.sdk.api.model.Shop;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import io.reactivex.rxjava3.core.Single;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
+
+public class RepositoryTest implements RandomTestData {
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+    private Repository classUnderTest;
+    @Mock
+    private EntersektApi mockApi;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        classUnderTest = new Repository(mockApi);
+    }
+
+    @Test
+    public void getAllCities() {
+        // GIVEN
+        List<City> cities = Arrays.asList(randomCity(), randomCity());
+        when(mockApi.getCities()).thenReturn(Single.just(cities));
+
+        // WHEN
+        List<City> results = classUnderTest.getAllCities().blockingGet();
+
+        // THEN
+        assertArrayEquals(cities.toArray(), results.toArray());
+    }
+
+    @Test
+    public void getSpecificCityWhenItExists() {
+        // GIVEN
+        City expectedCity = randomCity();
+        when(mockApi.getCities()).thenReturn(Single.just(Arrays.asList(randomCity(), expectedCity, randomCity())));
+
+        // WHEN
+        City result = classUnderTest.getCity(expectedCity.getName()).blockingGet();
+
+        // THEN
+        assertEquals(expectedCity, result);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void getSpecificCityWhenItDoesntExist() {
+        // GIVEN
+        String cityName = "I don't exist";
+        when(mockApi.getCities()).thenReturn(Single.just(Arrays.asList(randomCity(), randomCity())));
+
+        // WHEN
+        City result = classUnderTest.getCity(cityName).blockingGet();
+
+        // THEN
+        fail("Exception should have occurred");
+    }
+
+    @Test
+    public void getMallsOfCityWhenThereAre() {
+        // GIVEN
+        List<Mall> expectedMalls = Arrays.asList(randomMall(), randomMall());
+        City city = randomCity(expectedMalls.toArray(new Mall[0]));
+        when(mockApi.getCities()).thenReturn(Single.just(Collections.singletonList(city)));
+
+        // WHEN
+        List<Mall> malls = classUnderTest.getMallsForCity(city.getName()).blockingGet();
+
+        // THEN
+        assertArrayEquals(expectedMalls.toArray(), malls.toArray());
+    }
+
+    @Test
+    public void getMallsOfCityWhenThereAreNone() {
+        // GIVEN
+        City city = randomCity();
+        when(mockApi.getCities()).thenReturn(Single.just(Collections.singletonList(city)));
+
+        // WHEN
+        List<Mall> malls = classUnderTest.getMallsForCity(city.getName()).blockingGet();
+
+        // THEN
+        assertTrue(malls.isEmpty());
+    }
+
+    @Test
+    public void getSpecificMallInCityWhenItExists() {
+        // GIVEN
+        Mall expectedMall = randomMall();
+        City city = randomCity(randomMall(), expectedMall, randomMall());
+        when(mockApi.getCities()).thenReturn(Single.just(Collections.singletonList(city)));
+
+        // WHEN
+        Mall result = classUnderTest.getMallInCity(city.getName(), expectedMall.getName()).blockingGet();
+
+        // THEN
+        assertEquals(expectedMall, result);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void getSpecificMallInCityWhenItDoesntExists() {
+        // GIVEN
+        String mallName = "There isn't one";
+        City city = randomCity(randomMall(), randomMall());
+        when(mockApi.getCities()).thenReturn(Single.just(Collections.singletonList(city)));
+
+        // WHEN
+        Mall result = classUnderTest.getMallInCity(city.getName(), mallName).blockingGet();
+
+        // THEN
+        fail("Exception should have occurred");
+    }
+
+    @Test
+    public void getShopsOfMallWhenThereAre() {
+        // GIVEN
+        List<Shop> expectedShops = Arrays.asList(randomShop(), randomShop());
+        Mall mall = randomMall(expectedShops.toArray(new Shop[0]));
+        City city = randomCity(mall);
+        when(mockApi.getCities()).thenReturn(Single.just(Collections.singletonList(city)));
+
+        // WHEN
+        List<Shop> shops = classUnderTest.getShopsForMall(city.getName(), mall.getName()).blockingGet();
+
+        // THEN
+        assertArrayEquals(expectedShops.toArray(), shops.toArray());
+    }
+
+    @Test
+    public void getShopsOfMallWhenThereAreNone() {
+        // GIVEN
+        Mall mall = randomMall();
+        City city = randomCity(mall);
+        when(mockApi.getCities()).thenReturn(Single.just(Collections.singletonList(city)));
+
+        // WHEN
+        List<Shop> shops = classUnderTest.getShopsForMall(city.getName(), mall.getName()).blockingGet();
+
+        // THEN
+        assertTrue(shops.isEmpty());
+    }
+
+    @Test
+    public void getSpecificShopInMallWhenItExists() {
+        // GIVEN
+        Shop expectedShop = randomShop();
+        Mall mall = randomMall(randomShop(), expectedShop, randomShop());
+        City city = randomCity(mall);
+        when(mockApi.getCities()).thenReturn(Single.just(Collections.singletonList(city)));
+
+        // WHEN
+        Shop result = classUnderTest.getShopInMall(city.getName(), mall.getName(), expectedShop.getName()).blockingGet();
+
+        // THEN
+        assertEquals(expectedShop, result);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void getSpecificShopInMallWhenItDoesntExists() {
+        // GIVEN
+        String shopName = "There isn't one";
+        Mall mall = randomMall(randomShop(), randomShop());
+        City city = randomCity(mall);
+        when(mockApi.getCities()).thenReturn(Single.just(Collections.singletonList(city)));
+
+        // WHEN
+        Shop result = classUnderTest.getShopInMall(city.getName(), mall.getName(), shopName).blockingGet();
+
+        // THEN
+        fail("Exception should have occurred");
+    }
+
+    @Test
+    public void getShopsForCity() {
+        // GIVEN
+        Shop[] allShops = new Shop[]{randomShop(), randomShop(), randomShop(), randomShop()};
+        City city = randomCity(randomMall(allShops[0], allShops[1]), randomMall(allShops[2], allShops[3]));
+        when(mockApi.getCities()).thenReturn(Single.just(Collections.singletonList(city)));
+
+        // WHEN
+        List<Shop> results = classUnderTest.getShopsForCity(city.getName()).blockingGet();
+
+        // THEN
+        assertArrayEquals(allShops, results.toArray());
+    }
+}
